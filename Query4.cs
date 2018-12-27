f5356521411a3c238666b12285883ad1cdb9fac2
ValerSiya <valeriesuvorova88@gmail.com>
2018-12-27 17:54:17 +0200

    Update Query4.cs

--- Query4.cs
+++ Query4.cs
@@ -13,9 +13,9 @@ namespace Zalik
             Console.WriteLine("Реализация запроса 4");
             Console.Write("4.Введите название сувенира:");
             string nm = Console.ReadLine();
-            Console.Write("Введите дату(типа 2018-02-02):");
-            string dt = Console.ReadLine();
-            string sql4 = "SELECT Maker.Id, Maker.Name, Maker.Country FROM Souvenir INNER JOIN Maker on Maker.Id = Souvenir.MakerId WHERE Souvenir.Name='" + nm + "' AND Data='" + dt + "'";
+            Console.Write("Введите год:");
+             int dt = Convert.ToInt32(Console.ReadLine());
+            string sql4 = "SELECT Maker.Id, Maker.Name, Maker.Country FROM Souvenir INNER JOIN Maker on Maker.Id = Souvenir.MakerId WHERE Souvenir.Name='" + nm + "' AND YEAR(Data)=" + dt + "";
             Task(sql4, c);
             Console.ReadKey();
         }
