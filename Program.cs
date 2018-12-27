bb387866efb77ac5e07c34ef8e9d34c0221860ff
ValerSiya <valeriesuvorova88@gmail.com>
2018-12-27 17:30:30 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -29,91 +29,24 @@ namespace Zalik
                 connection.Close();   
             }*/
             Connection c = new Connection();
-            c.Launch();
-            Console.Write("1.Введите название: ");
-            string name = Console.ReadLine();
-            string sql1 = "SELECT  Souvenir.Name, Souvenir.Data, Souvenir.Price FROM Souvenir INNER JOIN Maker ON Maker.Id=Souvenir.MakerId  Where Maker.Name=" + "'" + name + "'";
-            Task2(sql1, c);
-            Console.ReadKey();
+            c.ConnectToBD();
+            Query1 q1 = new Query1();
+            q1.Realization(c);
             Console.WriteLine();
-            Console.Write("2.Введите страну: ");
-            string country = Console.ReadLine();
-            string sql2 = "SELECT * FROM Souvenir INNER JOIN Maker ON Maker.Id=Souvenir.MakerId  Where Maker.Country=" + "'" + country + "'";
-            Task2(sql2, c);
-            Console.ReadKey();
+            Query2 q2 = new Query2();
+            q2.Realization(c);
             Console.WriteLine();
-            Console.Write("3.Введите цену: ");
-            int price = Convert.ToInt32(Console.ReadLine());
-            string sql3 = "SELECT Maker.Name, Maker.Country from Souvenir join Maker on Maker.Id = Souvenir.MakerId WHERE Price <" + price + " group by Maker.Name, Country;";
-            Task2(sql3, c);
-            Console.ReadKey();
+            Query3 q3 = new Query3();
+            q3.Realization(c);
             Console.WriteLine();
-            Console.Write("4.Введите название сувенира:");
-            string nm = Console.ReadLine();
-            Console.Write("Введите дату(типа 2018-02-02):");
-            string dt = Console.ReadLine();
-            string sql4 = "SELECT Maker.Name, Maker.Country FROM Souvenir INNER JOIN Maker on Maker.Id = Souvenir.MakerId WHERE Souvenir.Name='"+nm+"' AND Data='"+dt+"'";
-            Task2(sql4, c);
-
-            Console.Write("5.Введите Id для удаления:");
-            int id = Convert.ToInt32(Console.ReadLine());
-            string sql5 = "DELETE  FROM Souvenir WHERE MakerId='" + id + "'; DELETE FROM Maker WHERE Id='" + id + "';";
-                 Taskdel(sql5, c);
+            Query4 q4 = new Query4();
+            q4.Realization(c);
+            Console.WriteLine();
+            Query5 q5 = new Query5();
+            q5.Realization(c);
+            Console.WriteLine();
 
             }
-
-
-        static void Show( SqlDataReader reader)
-        {
-            Console.Write("No.  ");
-            for (int i = 0; i < reader.FieldCount; i++)
-                Console.Write(reader.GetName(i) + "           ");
-            Console.WriteLine();
-            if (reader.HasRows)
-            {
-                int index = 0;
-                while (reader.Read())
-                {
-                    index++;
-                    Console.Write(index + ".");
-                    for (int i = 0; i < reader.FieldCount; i++)
-                        Console.Write("  " + reader.GetValue(i));
-                    Console.WriteLine();
-                }
-            }
-            else
-                Console.WriteLine("Table is empty");
-        }
-
-
-
-        static void Task2(string sql, Connection c)
-        {
-            using (SqlConnection connection = new SqlConnection(c.OS.Name))
-            {
-                connection.Open();
-            SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
-            DataSet ds = new DataSet();
-            adapter.Fill(ds);
-            foreach (DataRow row in ds.Tables[0].Rows)
-                Console.WriteLine("{0} {1}", row[0], row[1]);
-            connection.Close();
-            }
-            
-        }
-        static void Taskdel(string sql, Connection c)
-        {
-            using (SqlConnection connection = new SqlConnection(c.OS.Name))
-            {
-                connection.Open();
-                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
-                DataSet ds = new DataSet();
-                adapter.Fill(ds);
-                Console.WriteLine("Deleted!");
-                connection.Close();
-            }
-
-        }
 
         }
     }
